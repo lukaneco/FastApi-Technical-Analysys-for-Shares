bd0cf9727592c91b1acd7ace4756f0d6e1361858
Luca <32082555+lukaneco@users.noreply.github.com>
2020-09-30 18:01:56 -0300

    asd

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 docker build -t myimage .
 #docker run -d --name mycontainer -p 80:80 myimage
 docker run -it --name mycontainer -p 80:80 myimage
+# web: uvicorn fastapp:app --host 0.0.0.0 --port 80
