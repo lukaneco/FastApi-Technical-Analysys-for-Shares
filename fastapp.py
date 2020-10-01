26cb22ecc021809d5ca6374b4107fc008c31b375
Luca <32082555+lukaneco@users.noreply.github.com>
2020-10-01 20:58:02 -0300

    update cors

--- fastapp.py
+++ fastapp.py
@@ -63,7 +63,9 @@ origins = [
     "http://127.0.0.1:8080",
     "http://127.0.0.1:5500",
     "http://127.0.0.1:5501",
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "http://argenshares.herokuapp.com",
+    "https://argenshares.herokuapp.com"
 ]
 
 app.add_middleware(
